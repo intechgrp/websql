03a920882ab9bb559722f9283feeac5a96733f99
Antoine Detante <antoine.detante@intech.lu>
2012-12-21 09:48:33 +0100

    Migration to Play 2.1-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,18 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "WebSql"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "WebSql"
+  val appVersion      = "2.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+  	anorm,
+  	jdbc
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+  )
 
 }
