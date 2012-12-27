9cfa63977639df1502d737eabf4ff99056c1a0ef
Fabrice Croiseaux <fcroiseaux@gmail.com>
2012-12-27 09:15:29 +0100

    Reference to anorm removed in Project

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,6 @@ object ApplicationBuild extends Build {
     "com.typesafe" % "slick_2.10.0-RC1" % "0.11.2",
     "org.slf4j" % "slf4j-nop" % "1.6.4",
     "com.h2database" % "h2" % "1.3.166",
-  anorm,
   jdbc
   )
 
