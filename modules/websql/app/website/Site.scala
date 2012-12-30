d2633118551edfba2b227571c1cb3f9a723d325f
Antoine Detante <antoine.detante@intech.lu>
2012-12-30 19:54:25 +0100

    Add configuration parameter for SiteDesc class name

--- modules/websql/app/website/Site.scala
+++ modules/websql/app/website/Site.scala
@@ -6,19 +6,33 @@ import slick.session.Database
 import play.Play
 
 trait Website{
-  def title:String
-  def mainMenu:Map[String,String]
-  def pages:List[Page]
-  def authentication:String=null
-  def dbUrl:String
-  def dbDriver:String
+   def title:String
+   def mainMenu:Map[String,String]
+   def pages:List[Page]
+   def authentication:String=null
+   def dbUrl:String
+   def dbDriver:String
 
-  def getPage(key:String):Option[Page] = pages.find(_.id == key)
-  def hasAuthentication:Boolean = authentication != null
+   def getPage(key:String):Option[Page] = pages.find(_.id == key)
+   def hasAuthentication:Boolean = authentication != null
 }
 
 object Site{
-   lazy val WebSite:Website=Play.application().classloader().loadClass("website.SiteDesc").newInstance.asInstanceOf[Website]
+
+  /*
+  TODO : use structural type ??
+  type Website = {
+    def title:String
+    def mainMenu:Map[String,String]
+    def pages:List[Page]
+    def authentication:String
+    def dbUrl:String
+    def dbDriver:String
+  }*/
+
+
+  import play.api.Play.current
+  lazy val WebSite:Website=Play.application().classloader().loadClass(play.api.Play.configuration.getString("sitedesc").getOrElse("website.SiteDesc")).newInstance.asInstanceOf[Website]
 
   implicit lazy val db = Database.forURL(WebSite.dbUrl, driver = WebSite.dbDriver)
 }
